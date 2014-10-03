af6dca7a2ef2b2a14f77539f4e337f2b8d4c7d89
Irene <Irene@Irene-PC>
2014-10-03 07:40:29 -0600

    Modificación

---
+++ HojaDeTrabajo8/src/hojadetrabajo8/SimpleSet.java
@@ -0,0 +1,33 @@
+package hojadetrabajo8;
+
+/*
+UVG
+Algoritmos y Estructuras de Datos - 2011
+Hoja de trabajo 7
+Autor: Eduardo Castellanos
+
+Descripción: SimpleSet ejemplo con ArrayList.
+*/
+import java.util.ArrayList;
+
+class SimpleSet implements WordSet
+{
+	private ArrayList<Word> base;
+	
+	public SimpleSet()
+	{
+		base = new ArrayList<Word>();
+	}
+	
+	public Word get(Word word)
+	{
+		int index = base.indexOf(word);
+		if(index == -1) return null;
+		return base.get(index);
+	}
+	
+	public void add(Word wordObject)
+	{
+		base.add(wordObject);
+	}
+}
